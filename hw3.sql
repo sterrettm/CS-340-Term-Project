c520aa6a61c3bcaf5e0bf5784a2abdceb57b86ec
Matthew Sterrett <sterretm@oregonstate.edu>
2020-05-06 10:25:12 -0700

    Fixed foreign key constraints

--- hw3.sql
+++ hw3.sql
@@ -13,12 +13,14 @@ CREATE TABLE `Users`(
 CREATE TABLE `UserPhoneNumbers`(
     `userID` int(11) NOT NULL,
     `phoneNumber` varchar(32) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID),
     PRIMARY KEY (userID, phoneNumber)
 )engine=InnoDB;
 
 CREATE TABLE `UserLocations`(
     `userID` int(11) NOT NULL,
     `location` varchar(256) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID),
     PRIMARY KEY (userID, location)
 )engine=InnoDB;
 
@@ -28,13 +30,15 @@ CREATE TABLE `PrivateMessages`(
     `sendUserID` int(11) NOT NULL,
     `recvUserID` int(11) NOT NULL,
     `meesage` varchar(1024) NOT NULL,
-    FOREIGN KEY (sendUserID) REFERENCES Users(id),
-    FOREIGN KEY (recvUserID) REFERENCES Users(id)
+    FOREIGN KEY (sendUserID) REFERENCES Users(userID),
+    FOREIGN KEY (recvUserID) REFERENCES Users(userID)
 )engine=InnoDB;
 
 CREATE TABLE `Friends`(
     `userID1` int(11) NOT NULL,
     `userID2` int(11) NOT NULL,
+    FOREIGN KEY (userID1) REFERENCES Users(userID),
+    FOREIGN KEY (userID2) REFERENCES Users(userID),
     PRIMARY KEY (userID1, userID2)
 )engine=InnoDB;
 
