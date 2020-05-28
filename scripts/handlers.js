580f157d0cd86feb0670ce71c45aed0526db7f02
Matthew Sterrett <sterretm@oregonstate.edu>
2020-05-27 21:43:24 -0700

    Added basic messages support (also committed friends view file)

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -16,7 +16,29 @@ function messages(req,res,otherUname,pool){
     if (res.locals.userID == -1){
         res.redirect('../login')
     }else{
-        
+        pool.query('SELECT userID FROM Users WHERE username=?',[otherUname],function(err,results,fields){
+            if (err){
+                res.sendStatus(500)
+            }else if (results.length != 1){
+                res.sendStatus(400)
+            }else{
+                var userID = res.locals.userID
+                var otherUserID = results[0].userID
+                pool.query('CALL getMessages(?,?);',[userID,otherUserID],function(err,results,fields){
+                    if (err){
+                        res.sendStatus(500)
+                    }else{
+                        var messages = []
+                        for (var i = 0; i < results[0].length; i++){
+                            messages.push({})
+                            messages[i].message = results[0][i].message
+                            messages[i].side = {sent: "left", recv: "right"}[results[0][i].sentOrRecv]
+                        }
+                        res.status(200).render("messages",{messages: messages, otherUname: otherUname})
+                    }
+                })
+            }
+        })
     }
 }
 
