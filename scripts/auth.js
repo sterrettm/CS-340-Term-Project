e90cc5628e78762d3778ab794730de35144d189f
Matthew Sterrett <sterretm@oregonstate.edu>
2020-05-29 19:49:17 -0700

    Changed from bcrypt to bcryptjs for compatibility with flip servers insanely old software

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 function validateSession(req, res, pool, callback){
     // First, set default values for userID and username
